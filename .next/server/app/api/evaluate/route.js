7a679fbc658af2b24db5b5902bf5e736ae89a65f
tjm5f <g.tajimi@flux-g.com>
2025-08-02 13:31:43 +0900

    remove .next and updates

--- .next/server/app/api/evaluate/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/evaluate/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_b714d94d._.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__79d4a93d._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/evaluate/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/evaluate/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/evaluate/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
